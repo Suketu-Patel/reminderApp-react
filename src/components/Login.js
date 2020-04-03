5915da69cc3aa263ac25a56331199ebf963efb94
suketu-patel <suketupatel29@gmail.com>
2020-04-03 18:46:07 +0530

    added login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,14 @@
 import React from "react"
 import './style.css'
-const Signup = () => {
+const Login = () => {
     return (
         <div style={{ height:"100vh",overflow:'hidden'}}>
             <div className="container" style={{marginTop:100}}>
             <form style={{width:"70%", margin:'0 auto'}}>
-            <h2 className="mb-5" style={{margin:"0 auto", textAlign:"center"}}>Create Your Account</h2>
+            <h2 className="mb-5" style={{margin:"0 auto", textAlign:"center"}}>Log In</h2>
                 <div className="form-group row">
                     <div className="col">
-                        <input type="text" readonly className="inputBtn form-control" placeholder="Enter your Name"/>
-                    </div>
-                </div>
-                <div className="form-group row">
-                    <div className="col">
-                        <input type="text" readonly className="inputBtn form-control" placeholder="Enter your email"/>
+                        <input type="text" className="inputBtn form-control" placeholder="Enter your email"/>
                     </div>
                 </div>
                 <div className="form-group row">
@@ -23,7 +18,7 @@ const Signup = () => {
                 </div>
                 <div className="form-group row">
                     <div className="col">
-                    <input className="btn btn-primary inputBtn form-control" type="Submit"  defaultValue="Register"></input>
+                    <input className="btn btn-primary inputBtn form-control" type="Submit" defaultValue="Log In"></input>
                     </div>
                 </div>
             </form>
@@ -31,4 +26,4 @@ const Signup = () => {
         </div>
     )
 }
-export default Signup
+export default Login
