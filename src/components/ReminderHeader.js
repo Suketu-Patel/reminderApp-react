96543274fe2e6ddb0b01fedf221e7ce3a001fdc3
suketu-patel <suketupatel29@gmail.com>
2020-04-03 18:36:52 +0530

    signup added

--- src/components/ReminderHeader.js
+++ src/components/ReminderHeader.js
@@ -7,8 +7,8 @@ const ReminderHeader = () => {
         <nav className="navbar navbar-dark bg-primary">
             <NavLink to={"/"} className="navbar-brand">Location Reminder</NavLink>
             <div className="form-inline">
-                <NavLink to={"/"} className="navbar-item mr-3" style={{color:"white"}} >Login</NavLink>
-                <NavLink to={"/signup"} className="navbar-item" style={{color:"white"}} >Register</NavLink>
+                <NavLink to={"/"} className="navbar-item mr-3" activeStyle={{backgroundColor:"white",color:"black"}} style={{color:"white"}} >Login</NavLink>
+                <NavLink to={"/signup"} className="navbar-item" activeStyle={{backgroundColor:"white",color:"black"}} style={{color:"white"}} >Register</NavLink>
             </div>
         </nav>
     )
