7508aba7e753efb3d8ac819f01e558f77cd21435
suketu-patel <suketupatel29@gmail.com>
2020-04-14 19:28:05 +0530

    claimreject

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -26,17 +26,17 @@ const Signup = (props) => {
             <h2 className="mb-5" style={{margin:"0 auto", textAlign:"center"}}>Create Your Account</h2>
                 <div className="form-group row">
                     <div className="col">
-                        <input type="text" readOnly className="inputBtn form-control" placeholder="Enter your Name" name="name" onChange={setCreds}/>
+                        <input type="text" value={userCreds.name} className="inputBtn form-control" placeholder="Enter your Name" name="name" onChange={setCreds}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <div className="col">
-                        <input type="text" readOnly className="inputBtn form-control" placeholder="Enter your email" name="email" onChange={setCreds}/>
+                        <input type="text" value={userCreds.email} className="inputBtn form-control" placeholder="Enter your email" name="email" onChange={setCreds}/>
                     </div>
                 </div>
                 <div className="form-group row">
                     <div className="col">
-                        <input type="password" className="inputBtn form-control" id="inputPassword" placeholder="Enter your password" name="password" onChange={setCreds}/>
+                        <input type="password" value={userCreds.password} className="inputBtn form-control" id="inputPassword" placeholder="Enter your password" name="password" onChange={setCreds}/>
                     </div>
                 </div>
                 <div className="form-group row">
